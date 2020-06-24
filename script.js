f227cc7454080c5a781e0b619b4f352dbc7bb17d
Andrea <andreapirrello1990@gmail.com>
2020-06-24 15:53:57 +0200

    creata array e inserito prompt per l'utente

---
+++ script.js
@@ -0,0 +1,11 @@
+// Per l'esercizio di oggi, chiedete all'utente un cognome e aggiungetelo alla lista, poi ordinate, utilizzate una funzionalità di js per l'ordinamento.
+// Dopo aver ordinato, l'esercizio chiede l'indice del nuovo elemento inserito, per farlo utilizzate il metodo della ricerca dell'ultima esercitazione.
+
+// array creata con lista cognomi
+var listaCognomi = ["Tito","Cassati","De Rosa","Versace","Aiello"]
+console.log(listaCognomi);
+
+// prompt per l'utente che aggiunge il cognome
+var cognomeUtente = prompt("Inserisci il tuo Cognome: ");
+// push che mi aggiunge il cognomeUtente alla lista array
+listaCognomi.push(cognomeUtente);
